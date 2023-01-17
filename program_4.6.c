d74d445fe576e573749343684f57dd96cfd46a7d
Teona Johnson <Teona.Johnson.Codes@gmail.com>
2023-01-17 10:20:01 -0500

    Add files via upload

---
+++ program_4.6.c
@@ -0,0 +1,17 @@
+// Program to intriduce the while statement
+
+#include <stdio.h>
+
+int main () 
+
+{
+	int count = 1;
+
+	while ( count <= 5) 
+		{ 
+			printf ("%i\n", count);
+			++count;
+		}
+	return 0;
+
+} 
